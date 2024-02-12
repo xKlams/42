462050dfbc64e778eb96ec1786cc73ccc916282f
Federico De Sisti <federicodesisti@gmail.com>
2024-02-12 16:53:43 +0100

    test

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,18 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fde-sist <fde-sist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/06 16:50:57 by fde-sist          #+#    #+#             */
-/*   Updated: 2024/02/06 16:51:44 by fde-sist         ###   ########.fr       */
+/*   Created: 2024/02/07 12:53:37 by fde-sist          #+#    #+#             */
+/*   Updated: 2024/02/07 14:20:51 by fde-sist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit(int a)
+void	*ft_memcpy(void *dest, const void *src, size_t lenght)
 {
-	return (a <= '9' && a >= '0');
+	char		*aux_dest;
+	const char	*aux_src;
+
+	if(dest == NULL && src == NULL)
+		return NULL;
+	aux_dest = dest;
+	aux_src = src;
+	while(lenght--)
+		*(aux_dest + lenght) = *(aux_src + lenght);
+	return dest;
 }
--- libft/libft.h
+++ libft/libft.h
@@ -1,14 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fde-sist <fde-sist@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 16:51:10 by fde-sist          #+#    #+#             */
+/*   Updated: 2024/02/07 14:21:33 by fde-sist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_isalnum(int a);
-int	ft_isalpha(int a);
-int	ft_isascii(int a);
-int	ft_isdigit(int a);
-int	ft_isprint(int a);
-void	*memset(void *a, int c, size_t lenght);
-int	ft_strlen(char *string);
+int		ft_isalnum(int a);
+int		ft_isalpha(int a);
+int		ft_isascii(int a);
+int		ft_isdigit(int a);
+int		ft_isprint(int a);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *a, int c, size_t lenght);
+void	*ft_memcpy(void *dest, const void *src, size_t lenght);
 
 #endif
