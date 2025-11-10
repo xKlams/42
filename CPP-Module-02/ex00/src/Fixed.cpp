8055f058ab378a607392502a257640b54563d452
Federico De Sisti <federicodesisti@gmail.com>
2025-11-10 16:45:09 +0100

    ex00 cpp02

---
+++ CPP-Module-02/ex00/src/Fixed.cpp
@@ -0,0 +1,37 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void): _number_value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &src)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_number_value = src.getRawBits();
+
+	return (*this);
+}
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_number_value);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_number_value = raw;
+}
