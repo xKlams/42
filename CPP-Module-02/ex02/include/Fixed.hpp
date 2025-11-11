73b33e9ae761f68d47af42dec46367bd8abe2769
Federico De Sisti <federicodesisti@gmail.com>
2025-11-11 12:17:22 +0100

    ex02 started

---
+++ CPP-Module-02/ex02/include/Fixed.hpp
@@ -0,0 +1,45 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed 
+{
+	private:
+		int			_number_value;
+		static const int	_fractional_bits;
+	public:
+		Fixed();
+		Fixed(const Fixed& copy);
+		Fixed(const int num);
+		Fixed(const float num);
+		~Fixed();
+
+		Fixed &operator=(const Fixed &src);
+
+		float operator+(const Fixed rhs);
+		float operator-(const Fixed rhs);
+		float operator*(const Fixed rhs);
+		float operator/(const Fixed rhs);
+
+		Fixed& operator++();
+		Fixed operator++(int);
+		Fixed& operator--();
+		Fixed operator--(int);
+
+		bool operator>(const Fixed rhs) const;
+		bool operator<(const Fixed rhs) const;
+		bool operator>=(const Fixed rhs) const;
+		bool operator<=(const Fixed rhs) const;
+		bool operator==(const Fixed rhs) const;
+		bool operator!=(const Fixed rhs) const;
+
+		int getRawBits( void ) const;
+		void	setRawBits( int const raw );
+		float	toFloat (void ) const;
+		int	toInt( void ) const;
+};
+
+std::ostream    &operator<<(std::ostream &os, Fixed const &fixed);
+
+#endif
